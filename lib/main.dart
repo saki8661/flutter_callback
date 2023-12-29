643b62a7e81e45ed01fc3230eec9649508dc0ac7
saki8661 <saki8661@naver.com>
2023-12-29 09:48:56 +0900

    콜백생성 및 연결

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String childMessageContent = "여기는 부모 위젯 영역이야";
 
+  // 자식들에게 함수를 전달하자
+  void onCallbackPressed() {
+    // 화면에 렌더링 할려면 -->
+    setState(() {
+      childMessageContent = "자식에게 이벤트 발생 했네";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,8 +31,10 @@ class _MyAppState extends State<MyApp> {
           body: Column(
             children: [
               Expanded(child: Center(child: Text(childMessageContent))),
-              Expanded(flex: 1, child: ChildA()),
-              Expanded(flex: 1, child: ChildB()),
+              Expanded(flex: 1, child: ChildA(callback: onCallbackPressed)),
+              Expanded(flex: 1, child: ChildB(callback: onCallbackPressed)),
+              // 자식이 생성될 때 부모의 함수를 받음
+              // 자식에게 이벤트 발생하면 부모에게 콜백
             ],
           ),
         ),
@@ -34,16 +44,17 @@ class _MyAppState extends State<MyApp> {
 }
 
 class ChildA extends StatelessWidget {
-  const ChildA({super.key});
+  // 상태와, 기능
+  final VoidCallback callback;
+
+  const ChildA({required this.callback, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
-        onTap: () {
-          print("child A에 이벤트 발생");
-        },
+        onTap: callback,
         child: Container(
           width: double.infinity,
           color: Colors.orange,
@@ -55,16 +66,17 @@ class ChildA extends StatelessWidget {
 }
 
 class ChildB extends StatelessWidget {
-  const ChildB({super.key});
+
+  final VoidCallback callback;
+
+  const ChildB({required this.callback, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
-        onTap: () {
-          print("child B에 이벤트 발생");
-        },
+        onTap: callback,
         child: Container(
           width: double.infinity,
           color: Colors.red,
