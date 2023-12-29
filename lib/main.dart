0b08ba7ca20139102d2c8dd5c1a8d85a8c48dc8b
saki8661 <saki8661@naver.com>
2023-12-29 10:17:08 +0900

    콜백함수에 데이터 전달 받기

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,21 @@ class _MyAppState extends State<MyApp> {
   String childMessageContent = "여기는 부모 위젯 영역이야";
 
   // 자식들에게 함수를 전달하자
-  void onCallbackPressed() {
+  // 매개 변수를 받을 수 있도록 설계하자
+  // 함수의 설계 모양이 변경 됨
+  // (msg){}, --> String
+  // (context) {} --> BuildContext
+  void onCallbackPressed(String msg) {
     // 화면에 렌더링 할려면 -->
+    // setState --> build() 함수를 다시 실행해!!!
     setState(() {
-      childMessageContent = "자식에게 이벤트 발생 했네";
+      childMessageContent = msg;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print("부모 위젯에 빌드 함수가 다시 호출 되니?");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
@@ -31,8 +37,8 @@ class _MyAppState extends State<MyApp> {
           body: Column(
             children: [
               Expanded(child: Center(child: Text(childMessageContent))),
-              Expanded(flex: 1, child: ChildA(callback: onCallbackPressed)),
-              Expanded(flex: 1, child: ChildB(callback: onCallbackPressed)),
+              Expanded(flex: 1, child: ChildA(onCallback: onCallbackPressed)),
+              Expanded(flex: 1, child: ChildB(onCallback: onCallbackPressed)),
               // 자식이 생성될 때 부모의 함수를 받음
               // 자식에게 이벤트 발생하면 부모에게 콜백
             ],
@@ -45,16 +51,21 @@ class _MyAppState extends State<MyApp> {
 
 class ChildA extends StatelessWidget {
   // 상태와, 기능
-  final VoidCallback callback;
+  // final VoidCallback callback; // (String msg) {}
+  // Function() x
+  final Function(String msg) onCallback;
 
-  const ChildA({required this.callback, super.key});
+  const ChildA({required this.onCallback, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
-        onTap: callback,
+        // () {}, 타입이 다르다
+        onTap: (){
+          onCallback("안녕 여기는 A");
+        },
         child: Container(
           width: double.infinity,
           color: Colors.orange,
@@ -67,16 +78,17 @@ class ChildA extends StatelessWidget {
 
 class ChildB extends StatelessWidget {
 
-  final VoidCallback callback;
-
-  const ChildB({required this.callback, super.key});
+  final Function(String msg) onCallback;
+  const ChildB({required this.onCallback, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: InkWell(
-        onTap: callback,
+        onTap: (){
+          onCallback("하이 여기는 어딜까?");
+        },
         child: Container(
           width: double.infinity,
           color: Colors.red,
